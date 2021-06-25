8d00abfc9000fdd057ca0b294ed4075975845465
FIRSTQWERT <firstqwert@gmail.com>
2021-06-25 14:44:56 +0300

    Add .docx test

---
+++ src/test/java/tests/DocxFileTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static utils.Files.getDocx;
+
+public class DocxFileTest {
+@Test
+        void docxTest ( ) throws InvalidFormatException, IOException {
+                String docxFilePath = "src/test/resources/files/1.docx";
+                String expectedData = "Simple docx test";
+                String text = getDocx(docxFilePath);
+
+                assertThat(text, containsString(expectedData));
+        }
+}
--- src/test/java/utils/Files.java
+++ src/test/java/utils/Files.java
@@ -3,38 +3,47 @@ package utils;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Files {
     public static String readTextFromFile(File file) throws IOException {
         return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
     }
+
     public static String readTextFromPath(String path) throws IOException {
         return readTextFromFile(getFile(path));
     }
-    public static File getFile (String path) {
+
+    public static File getFile(String path) {
         return new File(path);
     }
-    public static PDF getPdf (String path) throws IOException {
+
+    public static PDF getPdf(String path) throws IOException {
         return new PDF(getFile(path));
     }
-    public static XLS getXls (String path) {
+
+    public static XLS getXls(String path) {
         return new XLS(getFile(path));
     }
+
     public static String getXlsx(String xlsxFilePath, int rowNumber, int cellNumber) throws IOException {
         File excelFile = new File(xlsxFilePath);
         FileInputStream fis = new FileInputStream(excelFile);
-
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
         XSSFSheet sheet = workbook.getSheetAt(0);
-
         Row row = sheet.getRow(rowNumber);
         String cell = row.getCell(cellNumber).toString();
 
@@ -43,4 +52,15 @@ public class Files {
         return cell;
     }
 
+    public static String getDocx(String docxFilePath) throws InvalidFormatException, IOException {
+        String text = "";
+        XWPFDocument doc = new XWPFDocument(OPCPackage.open(docxFilePath));
+        for (XWPFParagraph p : doc.getParagraphs()) {
+            List<XWPFRun> runs = p.getRuns();
+            for (XWPFRun r : runs) {
+                text += r.getText(0);
+            }
+        }
+        return text;
+    }
 }
